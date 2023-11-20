9efc3afb0f676a5e28add53da077b915e935df8b
Dilyaaaa <124158228+Dilyaaaa@users.noreply.github.com>
2023-11-20 17:48:47 +0600

    Update App.js

--- magazine/src/App.js
+++ magazine/src/App.js
@@ -6,17 +6,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchAndListPage from './pages/SearchAndListPage';
 
 function App() {
-
-
-  ReactDOM.render(
+ return(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SearchAndListPage />} />
         <Route path="/article/:id" element={<ArticleDetails />} />
       </Routes>
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
+    </BrowserRouter>
+   
+ )
 }
 
 
